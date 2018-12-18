b0ab4dc0c2150424ef34bb8cb718aa959269bdee
jiarong <guojiaro@gmail.com>
2018-12-18 05:06:35 -0500

    Add throwing an error when clustering results is imcomplete

--- scripts/mcclust2mothur-list-cutoff.py
+++ scripts/mcclust2mothur-list-cutoff.py
@@ -12,8 +12,6 @@ Convert mcclust.clust to mothur.list
 import sys
 import os
 
-import screed
-
 def makeMothurListFile(f,listFile,target_cutoff):
     """
     Convert mcclust.clust to mothur.list
@@ -32,61 +30,71 @@ def makeMothurListFile(f,listFile,target_cutoff):
     None
     """
     
-    fp = open(f)
-    fw = open(listFile, 'w')
-    target_cutoff = float(target_cutoff)
-    assert 0 <= target_cutoff <= 1
-    triger = False
+    with open(f) as fp, open(listFile, 'w') as fw:
+        target_cutoff = float(target_cutoff)
+        assert 0 <= target_cutoff <= 1
+        triger = False
 
-    d = {}
-    temp_cutoff = None
-    temp_total = None
-    temp_str = None
-    for line in fp:
-        if 'File' in line:
-            print line
-            continue
-        if 'Sequences:' in line:
-            print line
-            continue
-        line = line.strip()
-        if 'distance cutoff:' in line:
-            _cutoff = line.split(':',1)[1].strip()
-            continue
-        if 'Total Clusters:' in line:
-            total = line.split(':', 1)[1].strip()
-            total = int(total)
-            triger = True
-            continue
-
-        if triger:
-            if not line:
-                str = '\t'.join(d.values())
-                _cutoff = float(_cutoff)
-
-                if _cutoff == target_cutoff:
-                    print >> fw, '%.2f\t%d\t%s' %(target_cutoff, total, str)
-                    break
-                elif _cutoff > target_cutoff:
-                    print >> fw, '%.2f\t%d\t%s' \
-                                   %(target_cutoff, temp_total, temp_str)
-                    break
-
-                d = {}
-                triger = False
-                temp_cutoff = _cutoff
-                temp_total = total
-                temp_str = str
+        d = {}
+        temp_cutoff = None
+        temp_total = None
+        temp_str = None
+        _cutoff = None
+        for line in fp:
+            if 'File' in line:
+                print line
+                continue
+            if 'Sequences:' in line:
+                print line
+                continue
+            line = line.strip()
+            if 'distance cutoff:' in line:
+                _cutoff = line.split(':',1)[1].strip()
+                continue
+            if 'Total Clusters:' in line:
+                total = line.split(':', 1)[1].strip()
+                total = int(total)
+                triger = True
                 continue
 
-            assert len(line.split('\t')) == 4, 'parsing wrong ..'
-            cluNum, s, num, names = line.split('\t')
-            cluNum = float(cluNum)
-            Sname = ','.join(names.split())
-            if d.has_key(cluNum):
-                d[cluNum] = d[cluNum]+','+Sname
-            else:
-                d[cluNum] = Sname
+            if triger:
+                if not line:
+                    str = '\t'.join(d.values())
+                    _cutoff = float(_cutoff)
+
+                    if _cutoff == target_cutoff:
+                        print >> fw, '%.2f\t%d\t%s' %(target_cutoff, total, str)
+                        break
+                    elif _cutoff > target_cutoff:
+                        print >> fw, '%.2f\t%d\t%s' \
+                                    %(target_cutoff, temp_total, temp_str)
+                        break
+
+                    d = {}
+                    triger = False
+                    temp_cutoff = _cutoff
+                    temp_total = total
+                    temp_str = str
+                    continue
+
+                assert len(line.split('\t')) == 4, 'parsing wrong ..'
+                cluNum, s, num, names = line.split('\t')
+                cluNum = float(cluNum)
+                Sname = ','.join(names.split())
+                if d.has_key(cluNum):
+                    d[cluNum] = d[cluNum]+','+Sname
+                else:
+                    d[cluNum] = Sname
+
+    if _cutoff == None:
+        sys.stderr.write('*** {} is empty..'.format(f))
+        sys.exit(1)
+    elif _cutoff < target_cutoff:
+        mes = ('*** {} does not have clustering results with distance cutoff '
+                'as low as {}. Rerun with high Java_xmx might give clustering '
+                'results of cutoffs')
+        sys.stderr.write(mes.foramt(f, target_cutoff))
+        sys.exit(1)
 
 def main():
 
